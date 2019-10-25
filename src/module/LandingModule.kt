24c3360474e50ca66b3fe04d792860fa32561abb
martinhuszti <martinhuszti@gmail.com>
2019-10-25 13:40:49 +0200

    Connected to MongoDb

--- src/module/LandingModule.kt
+++ src/module/LandingModule.kt
@@ -1,14 +1,22 @@
 package hu.martinhuszti.module
 
+import hu.martinhuszti.url
 import io.ktor.application.Application
 import io.ktor.application.call
 import io.ktor.response.respondText
 import io.ktor.routing.get
 import io.ktor.routing.routing
+import org.litote.kmongo.coroutine.coroutine
+import org.litote.kmongo.reactivestreams.KMongo
 
 fun Application.landingModule() {
+
+
     routing {
         get("/") {
+            val client = KMongo.createClient(url).coroutine //use coroutine extension
+            val database = client.getDatabase("test") //normal java driver usage
+            database.createCollection("asd")
             call.respondText { "das" }
         }
     }
